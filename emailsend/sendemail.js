41b3ae51107f5b6f87a522f72c2ca355d13a7ac4
ShabahatAbbas5 <demotesting115@gmail.com>
2024-08-01 01:20:25 +0500

    Email Send

--- emailsend/sendemail.js
+++ emailsend/sendemail.js
@@ -13,48 +13,46 @@ async function sendEmail(to, pdfUrl, formtitle) {
     }
   });
 
-  try {
-    // Download PDF from S3
-    const response = await axios({
-      url: pdfUrl,
-      responseType: 'stream'
-    });
+  // Download PDF from S3
+  // const response = await axios({
+  //   url: pdfUrl,
+  //   responseType: 'stream'
+  // });
 
-    const tempFilePath = path.join(os.tmpdir(), formtitle + ' Form.pdf');
-    const writer = fs.createWriteStream(tempFilePath);
+  // const tempFilePath = path.join(os.tmpdir(), formtitle + ' Form.pdf');
+  // const writer = fs.createWriteStream(tempFilePath);
 
-    response.data.pipe(writer);
+  // response.data.pipe(writer);
 
-    await new Promise((resolve, reject) => {
-      writer.on('finish', resolve);
-      writer.on('error', reject);
-    });
+  // return new Promise((resolve, reject) => {
+  //   writer.on('finish', () => {
+  //     const mailOptions = {
+  //       from: process.env.GMAIL_USER,
+  //       to,
+  //       subject: formtitle + ' Form Submission',
+  //       text: 'Please find the attached PDF of your ' + formtitle + ' form submission.',
+  //       attachments: [
+  //         {
+  //           filename: formtitle + ' Form.pdf',
+  //           path: tempFilePath
+  //         }
+  //       ]
+  //     };
 
-    // const mailOptions = {
-    //   from: process.env.GMAIL_USER,
-    //   to,
-    //   subject: formtitle + ' Form Submission',
-    //   text: 'Please find the attached PDF of your ' + formtitle + ' form submission.',
-    //   attachments: [
-    //     {
-    //       filename: formtitle + ' Form.pdf',
-    //       path: tempFilePath
-    //     }
-    //   ]
-    // };
+  //     transporter.sendMail(mailOptions, (error, info) => {
+  //       if (error) {
+  //         return reject(error);
+  //       }
+  //       console.log('Email sent:', info.response);
+  //       fs.unlink(tempFilePath, () => {}); // Clean up temp file
+  //       resolve();
+  //     });
+  //   });
 
-    // await transporter.sendMail(mailOptions);
-    // console.log('Email sent successfully.');
-
-    fs.unlink(tempFilePath, (err) => {
-      if (err) {
-        console.error('Error deleting temp file:', err);
-      }
-    });
-  } catch (error) {
-    console.error('Error in sendEmail function:', error);
-    throw error; // Rethrow the error to be handled by caller
-  }
+  //   writer.on('error', (error) => {
+  //     reject(error);
+  //   });
+  // });
 }
 
 module.exports = sendEmail;
